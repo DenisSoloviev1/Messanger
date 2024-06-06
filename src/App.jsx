2f35bf2f07b73603e39c359e1c9f2271580351bd
DenisSoloviev1 <solovievdenis2004@gmail.com>
2024-06-07 01:12:08 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import "./components/DartMode.scss";
 import Authorization from "./pages/Authorization/Authorization.jsx";
 import { BrowserRouter } from "react-router-dom";
-import {useState} from 'react'
-
 
 export default function App() {
   const [activeMode, setActiveMode] = useState();
+
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh * 100}px`);
+    };
+
+    setVh();
+
+    window.addEventListener('resize', setVh);
+    return () => {
+      window.removeEventListener('resize', setVh);
+    };
+  }, []);
+
   return (
     <BrowserRouter>
-      <Authorization setActiveMode={setActiveMode} activeMode={activeMode}  className="--dart-mode"/>
+      <div className="app-container">
+        <Authorization setActiveMode={setActiveMode} activeMode={activeMode} className="--dart-mode"/>
+      </div>
     </BrowserRouter>
   );
 }
